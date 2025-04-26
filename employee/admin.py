e20fcbd8699f15d2448c6269fea7eab9860f4353
Rishmo <sweetmoonsahu@gmail.com>
2025-04-26 23:08:02 +0530

    initial commit

---
+++ employee/admin.py
@@ -0,0 +1,63 @@
+"""
+admin.py
+
+This page is used to register the model with admins site.
+"""
+
+from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+
+from employee.models import (
+    Actiontype,
+    BonusPoint,
+    DisciplinaryAction,
+    Employee,
+    EmployeeBankDetails,
+    EmployeeNote,
+    EmployeeTag,
+    EmployeeWorkInformation,
+    Policy,
+    PolicyMultipleFile,
+)
+
+# Register your models here.
+
+# admin.site.register(Employee)
+admin.site.register(EmployeeBankDetails)
+admin.site.register(EmployeeWorkInformation, SimpleHistoryAdmin)
+admin.site.register([EmployeeNote, EmployeeTag, PolicyMultipleFile, Policy, BonusPoint])
+admin.site.register([DisciplinaryAction, Actiontype])
+
+
+from django.contrib import admin
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        "badge_id",
+        "employee_first_name",
+        "employee_last_name",
+        "employee_user_id",
+        "is_active",
+    )
+
+    search_fields = (
+        "badge_id",
+        "employee_user_id__username",
+        "employee_first_name",
+        "employee_last_name",
+    )
+
+    list_filter = ("is_active",)
+
+    ordering = ("employee_first_name", "employee_last_name")
+
+    def delete_view(self, request, object_id, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context["custom_message"] = (
+            "Are you sure you want to delete this item? This action cannot be undone."
+        )
+        return super().delete_view(request, object_id, extra_context=extra_context)
+
+
+admin.site.register(Employee, EmployeeAdmin)
